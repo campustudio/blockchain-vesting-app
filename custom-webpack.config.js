e53a5be84599fa942f1fd3c32b4880cbb005dd09
Chris Du <hailong.du8889@gmail.com>
2025-12-02 01:47:07 +0800

    Update custom-webpack.config.js

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,27 +1,26 @@
-const webpack = require("webpack");
-require('@wwtc/dotenv').config();
+const webpack = require('webpack');
 
 const PUBLIC_ENV = {
-  API_URL: JSON.stringify(process.env.API_URL || ""),
-  APP_VERSION: JSON.stringify(process.env.APP_VERSION || ""),
+    API_URL: JSON.stringify(process.env.API_URL || ''),
+    APP_VERSION: JSON.stringify(process.env.APP_VERSION || ''),
 };
 
 module.exports = {
-  resolve: {
-    fallback: { "url": require.resolve("url/") }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: 'javascript/auto',
-      },
+    resolve: {
+        fallback: { url: require.resolve('url/') },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.mjs$/,
+                include: /node_modules/,
+                type: 'javascript/auto',
+            },
+        ],
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': PUBLIC_ENV,
+        }),
     ],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': PUBLIC_ENV
-    })
-  ]
-}
+};
